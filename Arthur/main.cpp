1ccf4e2e03181a24bc46d2e460e5f798fe448533
Rushil <rushil.kekre@gmail.com>
2017-05-09 23:40:27 -0700

    fixed viewing, translation and scaling issues for shaderball

--- Arthur/main.cpp
+++ Arthur/main.cpp
@@ -177,8 +177,8 @@ int main()
 
         // Draw the loaded model
         glm::mat4 model;
-        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
+        model = glm::translate(model, glm::vec3(0.0f, -1.0f, 0.0f)); // Translate it down a bit so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));	// It's a bit too big for our scene, so scale it down
         glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         ourModel.Draw(shader);
 
--- Arthur/shaders/skybox.vs
+++ Arthur/shaders/skybox.vs
@@ -5,9 +5,9 @@ out vec3 TexCoords;
 uniform mat4 projection;
 uniform mat4 view;
 
-
 void main()
 {
-    gl_Position =   projection * view * vec4(position, 1.0);  
+    vec4 pos =   projection * view * vec4(position, 1.0);  
+    gl_Position = pos.xyww;
     TexCoords = position;
 }  
