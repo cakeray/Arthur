587e402b3c8245813510d18931497b6d2e25f224
Rushil <rushil.kekre@gmail.com>
2017-06-01 17:24:06 -0700

    Included tangent, bitangent in the vertex struct and adding corresponding vertex attribute pointers

--- Arthur/Mesh.h
+++ Arthur/Mesh.h
@@ -1,7 +1,4 @@
 #pragma once
-#ifndef MESH_H
-#define MESH_H
-
 // Std. Includes
 #include <string>
 #include <fstream>
@@ -22,6 +19,10 @@ struct Vertex {
     glm::vec3 Normal;
     // TexCoords
     glm::vec2 TexCoords;
+    // Tangent
+    glm::vec3 Tangent;
+    // Bitangent
+    glm::vec3 Bitangent;
 };
 
 struct Texture {
@@ -55,6 +56,8 @@ public:
         // Bind appropriate textures
         GLuint diffuseNr = 1;
         GLuint specularNr = 1;
+        GLuint normalNr = 1;
+        GLuint heightNr = 1;
         for (GLuint i = 0; i < this->textures.size(); i++)
         {
             glActiveTexture(GL_TEXTURE0 + i); // Active proper texture unit before binding
@@ -66,6 +69,10 @@ public:
                 ss << diffuseNr++; // Transfer GLuint to stream
             else if (name == "texture_specular")
                 ss << specularNr++; // Transfer GLuint to stream
+            else if (name == "texture_normal")
+                ss << normalNr++; // Transfer GLuint to stream
+            else if (name == "texture_height")
+                ss << heightNr++; // Transfer GLuint to stream
             number = ss.str();
             // Now set the sampler to the correct texture unit
             glUniform1i(glGetUniformLocation(shader.Program, (name + number).c_str()), i);
@@ -123,12 +130,15 @@ private:
         // Vertex Texture Coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
+        // Vertex Tangent
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Tangent));
+        // Vertex Bitangent
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Bitangent));
 
         glBindVertexArray(0);
     }
 };
-
-
-#endif // !MESH_H
 
 
