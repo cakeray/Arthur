6af69c7ea107d7d3960f7f5f87e20967e3f1992e
Rushil <rushil.kekre@gmail.com>
2017-06-17 18:58:20 -0500

    Basic PBR workflow added (pre-texturing) UI elements added for metallic and roughness controls

--- Arthur/main.cpp
+++ Arthur/main.cpp
@@ -55,6 +55,7 @@ void Do_Movement();
 GLuint loadTexture(GLchar* path);
 void RenderCube();
 void RenderQuad();
+void RenderSphere();
 GLfloat lerp(GLfloat a, GLfloat b, GLfloat f);
 
 // ================================
@@ -94,10 +95,12 @@ float shineAmount = 0.25;
 ImVec4 lightColor = ImColor(255, 255, 255);
 
 // Rendering
-bool deferredRendering = true;
+bool deferredRendering = false;
 bool forwardRendering = false;
 bool deferredActive = true;
 bool ssaoActive = false;
+bool pbrActive = true;
+
 
 // SSAO
 int num_samples = 64;
@@ -105,6 +108,11 @@ int kernelSize = 64;
 float ssaoRadius = 0.5;
 float ssaoBias = 0.025;
 int power = 2;
+
+// PBR
+float metallic = 0.5;
+float roughness = 0.5;
+
 
 // ================================
 
@@ -184,6 +192,7 @@ int main()
     Shader modelLightingPass("shaders/model_lighting.vert", "shaders/model_lighting.frag");
     Shader ssaoShader("shaders/model_lighting.vert", "shaders/ssaoShader.frag");
     Shader ssaoBlurShader("shaders/model_lighting.vert", "shaders/ssaoBlur.frag");
+    Shader pbrShader("shaders/pbrShader.vs", "shaders/pbrShader.frag");
 
 
     GLfloat planeVertices[] = {
@@ -392,6 +401,14 @@ int main()
     ssaoBlurShader.Use();
     ssaoBlurShader.setInt("ssaoInput", 0);
 
+    // PBR Shader configuration
+    pbrShader.Use();
+    pbrShader.setVec3("albedo", 0.5f, 0.0f, 0.0f);
+    pbrShader.setFloat("ao", 1.0f);
+
+    glm::mat4 projection;
+    projection = glm::perspective(camera.Zoom, (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+    pbrShader.setMat4("projection", projection);
 
     // ================================
 
@@ -414,8 +431,6 @@ int main()
 
         glm::mat4 model;
         glm::mat4 view;
-        glm::mat4 projection;
-        projection = glm::perspective(camera.Zoom, (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
         view = camera.GetViewMatrix();
         model = glm::translate(model, glm::vec3(-0.5f, -1.0f, 0.0f)); // Translate it down a bit so it's at the center of the scene
         model = glm::rotate(model, rotationAngle, glm::vec3(rotX, rotY, rotZ));
@@ -423,6 +438,21 @@ int main()
 
         // GUI
         guiSetup();
+
+
+        if (pbrActive)
+        {
+            pbrShader.Use();
+            pbrShader.setMat4("view", view);
+            pbrShader.setVec3("camPos", camera.Position);
+            pbrShader.setFloat("metallic", metallic);
+            pbrShader.setFloat("roughness", roughness);
+            pbrShader.setVec3("lightPos", lightPos);
+            pbrShader.setVec3("lightColor", lightColor.x,lightColor.y,lightColor.z);
+            pbrShader.setMat4("model", model);
+            //RenderSphere();
+            ourModel.Draw(pbrShader);
+        }
 
         if (deferredRendering) 
         {
@@ -746,6 +776,18 @@ void guiSetup()
             ImGui::TreePop();
         }
     }
+    //PBR
+    if (ImGui::CollapsingHeader("PBR", 0))
+    {
+        if (ImGui::Button("Enable"))
+        {
+            pbrActive = true;
+            deferredRendering = false;
+            forwardRendering = false;
+        }
+        ImGui::SliderFloat("Metallic", &metallic, 0.0,1.0);
+        ImGui::SliderFloat("Roughness", &roughness, 0.0, 1.0);
+    }
     // About
     if (ImGui::CollapsingHeader("About", 0))
     {
@@ -775,6 +817,104 @@ GLfloat lerp(GLfloat a, GLfloat b, GLfloat f)
     return a + f * (b - a);
 }
 
+
+
+// renders (and builds at first invocation) a sphere
+// -------------------------------------------------
+unsigned int sphereVAO = 0;
+unsigned int indexCount;
+void RenderSphere()
+{
+    if (sphereVAO == 0)
+    {
+        glGenVertexArrays(1, &sphereVAO);
+
+        unsigned int vbo, ebo;
+        glGenBuffers(1, &vbo);
+        glGenBuffers(1, &ebo);
+
+        std::vector<glm::vec3> positions;
+        std::vector<glm::vec2> uv;
+        std::vector<glm::vec3> normals;
+        std::vector<unsigned int> indices;
+
+        const unsigned int X_SEGMENTS = 64;
+        const unsigned int Y_SEGMENTS = 64;
+        const float PI = 3.14159265359;
+        for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+        {
+            for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+            {
+                float xSegment = (float)x / (float)X_SEGMENTS;
+                float ySegment = (float)y / (float)Y_SEGMENTS;
+                float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+                float yPos = std::cos(ySegment * PI);
+                float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+
+                positions.push_back(glm::vec3(xPos, yPos, zPos));
+                uv.push_back(glm::vec2(xSegment, ySegment));
+                normals.push_back(glm::vec3(xPos, yPos, zPos));
+            }
+        }
+
+        bool oddRow = false;
+        for (int y = 0; y < Y_SEGMENTS; ++y)
+        {
+            if (!oddRow) // even rows: y == 0, y == 2; and so on
+            {
+                for (int x = 0; x <= X_SEGMENTS; ++x)
+                {
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                }
+            }
+            else
+            {
+                for (int x = X_SEGMENTS; x >= 0; --x)
+                {
+                    indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+                    indices.push_back(y       * (X_SEGMENTS + 1) + x);
+                }
+            }
+            oddRow = !oddRow;
+        }
+        indexCount = indices.size();
+
+        std::vector<float> data;
+        for (int i = 0; i < positions.size(); ++i)
+        {
+            data.push_back(positions[i].x);
+            data.push_back(positions[i].y);
+            data.push_back(positions[i].z);
+            if (uv.size() > 0)
+            {
+                data.push_back(uv[i].x);
+                data.push_back(uv[i].y);
+            }
+            if (normals.size() > 0)
+            {
+                data.push_back(normals[i].x);
+                data.push_back(normals[i].y);
+                data.push_back(normals[i].z);
+            }
+        }
+        glBindVertexArray(sphereVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+        float stride = (3 + 2 + 3) * sizeof(float);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, stride, (void*)(5 * sizeof(float)));
+    }
+
+    glBindVertexArray(sphereVAO);
+    glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+}
 
 // RenderQuad() Renders a 1x1 quad in NDC, best used for framebuffer color targets
 // and post-processing effects.
