8d0cef1e19d1e5b377e6d384858cee5aaef9c702
Rushil <rushil.kekre@gmail.com>
2017-06-28 20:18:16 -0500

    Removed tangent and bitangent from mesh structure. Removed texture detais from Draw() #endif my #ifndef

--- Arthur/Mesh.h
+++ Arthur/Mesh.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#ifndef MESH_H
+#define MESH_H
+
 // Std. Includes
 #include <string>
 #include <fstream>
@@ -19,16 +23,6 @@ struct Vertex {
     glm::vec3 Normal;
     // TexCoords
     glm::vec2 TexCoords;
-    // Tangent
-    glm::vec3 Tangent;
-    // Bitangent
-    glm::vec3 Bitangent;
-};
-
-struct Texture {
-    GLuint id;
-    string type;
-    aiString path;
 };
 
 class Mesh {
@@ -36,15 +30,13 @@ public:
     /*  Mesh Data  */
     vector<Vertex> vertices;
     vector<GLuint> indices;
-    vector<Texture> textures;
 
     /*  Functions  */
     // Constructor
-    Mesh(vector<Vertex> vertices, vector<GLuint> indices, vector<Texture> textures)
+    Mesh(vector<Vertex> vertices, vector<GLuint> indices)
     {
         this->vertices = vertices;
         this->indices = indices;
-        this->textures = textures;
 
         // Now that we have all the required data, set the vertex buffers and its attribute pointers.
         this->setupMesh();
@@ -53,47 +45,11 @@ public:
     // Render the mesh
     void Draw(Shader shader)
     {
-        // Bind appropriate textures
-        GLuint diffuseNr = 1;
-        GLuint specularNr = 1;
-        GLuint normalNr = 1;
-        GLuint heightNr = 1;
-        for (GLuint i = 0; i < this->textures.size(); i++)
-        {
-            glActiveTexture(GL_TEXTURE0 + i); // Active proper texture unit before binding
-                                              // Retrieve texture number (the N in diffuse_textureN)
-            stringstream ss;
-            string number;
-            string name = this->textures[i].type;
-            if (name == "texture_diffuse")
-                ss << diffuseNr++; // Transfer GLuint to stream
-            else if (name == "texture_specular")
-                ss << specularNr++; // Transfer GLuint to stream
-            else if (name == "texture_normal")
-                ss << normalNr++; // Transfer GLuint to stream
-            else if (name == "texture_height")
-                ss << heightNr++; // Transfer GLuint to stream
-            number = ss.str();
-            // Now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.Program, (name + number).c_str()), i);
-            // And finally bind the texture
-            glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
-        }
+        glActiveTexture(GL_TEXTURE0);
 
-        // Also set each mesh's shininess property to a default value (if you want you could extend this to another mesh property and possibly change this value)
-        glUniform1f(glGetUniformLocation(shader.Program, "material.shininess"), 16.0f);
-
-        // Draw mesh
         glBindVertexArray(this->VAO);
         glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
-
-        // Always good practice to set everything back to defaults once configured.
-        for (GLuint i = 0; i < this->textures.size(); i++)
-        {
-            glActiveTexture(GL_TEXTURE0 + i);
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
     }
 
 private:
@@ -130,15 +86,12 @@ private:
         // Vertex Texture Coords
         glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, TexCoords));
-        // Vertex Tangent
-        glEnableVertexAttribArray(3);
-        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Tangent));
-        // Vertex Bitangent
-        glEnableVertexAttribArray(4);
-        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)offsetof(Vertex, Bitangent));
 
         glBindVertexArray(0);
     }
 };
+
+
+#endif // !MESH_H
 
 
